ce405d9c6a9c22b3b9ad192453f9a9120a3f1528
nipunbandara <nipun.bandara@outlook.com>
2021-05-28 19:16:06 +0530

    UI added

---
+++ resources/assets/js/prod.js
@@ -0,0 +1,17 @@
+function productjs(){
+						document.getElementById("productfid").submit();
+						
+					}
+					
+					
+
+function profilefjs(){
+						document.getElementById("profilef").submit();
+						
+					}
+					
+function loggedindex(){
+
+						document.getElementById("logindex").submit();
+
+}
---
+++ resources/assets/prod.js
@@ -0,0 +1,18 @@
+function productjs(){
+						document.getElementById("productfid").submit();
+						
+					}
+					
+					
+
+function profilefjs(){
+						document.getElementById("profilef").submit();
+						
+					}
+					
+function loggedindex(){
+
+						alert("Hello! I am an alert box!!");
+
+
+}
